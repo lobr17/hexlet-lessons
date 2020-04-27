dd55d927707feb393d47d8ce937945f937a49491
Konstantin <konpichugin@yandex.ru>
2020-04-27 14:58:53 +0200

    first commit

---
+++ start.php
@@ -0,0 +1,21 @@
+<?php
+
+//require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
+/**
+ *
+ * ber
+ *
+ */
+
+use Funct\Strings\classify;
+
+use Stringy\Stringy as S;
+
+s('Camel-Case')->camelize();
+
+
+//classify('hello world');
+
+
+//echo 'ber';
